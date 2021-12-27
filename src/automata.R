2841da62a0e1aac70afb63f3d0e473475017501f
macosgrove <mary-anne.cosgrove@envato.com>
2021-12-27 15:51:39 +1100

    Raster image created and displayed. Slow!

--- src/automata.R
+++ src/automata.R
@@ -1,4 +1,4 @@
-requireNamespace("grid")
+require("grid")
 # https://stackoverflow.com/questions/28562288/how-to-use-the-hsl-hue-saturation-lightness-cylindric-color-model
 # specify h as whole input degrees (e.g 0-360)
 # s = 0.0 - 1 (0 - 100%)
@@ -11,46 +11,57 @@ hsl_to_rgb <- function(h, s, l) {
     r <- g <- b <- l
   } else {
     hue_to_rgb <- function(p, q, t) {
-      if (t < 0) { t <- t + 1.0 }
-      if (t > 1) { t <- t - 1.0 }
-      if (t < 1/6) { return(p + (q - p) * 6.0 * t) }
-      if (t < 1/2) { return(q) }
-      if (t < 2/3) { return(p + ((q - p) * ((2/3) - t) * 6)) }
+      if (t < 0) {
+        t <- t + 1.0
+      }
+      if (t > 1) {
+        t <- t - 1.0
+      }
+      if (t < 1 / 6) {
+        return(p + (q - p) * 6.0 * t)
+      }
+      if (t < 1 / 2) {
+        return(q)
+      }
+      if (t < 2 / 3) {
+        return(p + ((q - p) * ((2 / 3) - t) * 6))
+      }
       return(p)
     }
-    q <- ifelse(l < 0.5, l * (1.0 + s), l + s - (l*s))
+    q <- ifelse(l < 0.5, l * (1.0 + s), l + s - (l * s))
     p <- 2.0 * l - q
-    r <- hue_to_rgb(p, q, h + 1/3)
+    r <- hue_to_rgb(p, q, h + 1 / 3)
     g <- hue_to_rgb(p, q, h)
-    b <- hue_to_rgb(p, q, h - 1/3)
+    b <- hue_to_rgb(p, q, h - 1 / 3)
   }
-  return(rgb(r,g,b))
+  return(rgb(r, g, b))
 }
 
 # specify h, s and l in the range 0-100
-hsl100_to_rgb <- function(hsl) {
-  hsl_to_rgb(hsl[1]*360/100, hsl[2]/100, hsl[3]/100)
+hsl100_to_rgb <- function(h, s, l) {
+  hsl_to_rgb(h * 360 / 100, s / 100, l / 100)
 }
 
-setup <- function(){
+setup <- function(w=100, h=50) {
   quartz()
-  height <- 10
-  width <- 10
-  a42.viewport <<- grid::viewport(x=0.5, y=0.5, width=1, height=1, 
-                                  default.units="npc", xscale=c(1,10), yscale=c(1,10))
-  grid::pushViewport(a42.viewport)
-  init <<- c(45, 50, 50, 0)
-  universe <<- matrix(init, nrow = height, ncol = width)
+  width <<- w
+  height <<- h
+  hue <<- matrix((0:(width - 1)) * 100 / (width - 1), nrow = height, ncol = width, byrow = TRUE)
+  sat <<- matrix(100, nrow = height, ncol = width)
+  lum <<- matrix((0:(height - 1)) * 100 / (height - 1), nrow = height, ncol = width)
 }
 
-mainloop <- function(){
-  a42.params <- grid::gpar(fill="#fac0ff")
-  grid::grid.rect(x=1, y=1, width=2, height=3, gp=a42.params, vp=a42.viewport)
-  # universe_raster <- apply(universe, c(1,2), hsl100_to_rgb)
-  # grid.raster(universe_raster)
+mainloop <- function() {
+  universe_raster <<- matrix(0, height, width)
+  for (x in 1:height) {
+    for (y in 1:width) {
+      universe_raster[x, y] <- hsl100_to_rgb(hue[x, y], sat[x, y], lum[x, y])
+    }
+  }
+  grid.raster(universe_raster)
 }
 
-teardown <- function(){
+teardown <- function() {
   dev.off()
 }
 
