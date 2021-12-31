f11ab7b253604c52776db6b245dd025ed275202f
macosgrove <mary-anne.cosgrove@envato.com>
2021-12-31 12:39:26 +1100

    Experiment proving that grid.raster consumes memory with each iteration

--- src/experiments.R
+++ src/experiments.R
@@ -1,48 +1,27 @@
-require("grid")
+# This experiment shows that even without writing to the universe_raster
+# or modifying the hue/sat/lum rasters, memory usage increases and time to display
+# slows. This is a grid.raster issue.
 
-# Open a graphics window on Mac
-# quartz()
-
-
-# Various fills:
-
-hue <<- matrix(50, nrow = height, ncol = width) # red
-hue <<- matrix((0:(width - 1)) * 100 / (width - 1), nrow = height, ncol = width, byrow = TRUE) # vertical rainbow
-sat <<- matrix(0, nrow = height, ncol = width) # no colour
-sat <<- matrix(100, nrow = height, ncol = width) # full colour
-lum <<- matrix(runif(width*height)*100, nrow = height, ncol = width) # random greys
-lum <<- matrix((0:(height - 1)) * 100 / (height - 1), nrow = height, ncol = width) # black top, white bottom
-lum <<- matrix(((height - 1):0) * 100 / (height - 1), nrow = height, ncol = width) # white top, black bottom
-
-
-
-library(tcltk)
-
-mywait <- function() {
-  tt <- tktoplevel()
-  tkpack( tkbutton(tt, text='Continue', command={function()tkdestroy(tt)}),
-          side='bottom')
-  tkbind(tt,'<KeyPress-space>', function(){ print("You pressed space") } )
-  tkbind(tt,'<KeyPress-a>', function(){ print("You pressed a") } )
-  
-  tkwait.window(tt)
-}
-
-loop <- function() {
-  tt <- tktoplevel()
-  key <- NA
-  tkbind(tt,'<Key>', function(k) { key <<- k } )
-  quit <- FALSE
-  while (!quit) {
-    print(".")
-    if(!is.na(key)) {
-      print(paste("Key detected: ", key))
-      if (key == 20) {
-        quit <- TRUE
-      }
-      key <- NA
-    }
-    Sys.sleep(1)
+# Start: 
+# 349 MB
+# user  system elapsed 
+# 0.799   0.047   0.846 
+# End:
+# 438 MB
+# user  system elapsed 
+# 0.998   0.047   1.044   
+mem_experiment <- function () {
+  print(mem_used())
+  for (t in 1:30) {
+    time <- system.time({
+    # for (x in 1:height) {
+    #   for (y in 1:width) {
+    #     universe_raster[x, y] <- hsl100_to_rgb(hue[x, y], sat[x, y], lum[x, y])
+    #   }
+    # }
+    grid.raster(universe_raster, interpolate = FALSE)
+    print(mem_used())
+    print(time)
+    })
   }
-  tkdestroy(tt)
 }
