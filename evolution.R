f0c9b483d543ca3266c50eeded79c768ab67f54b
macosgrove <mary-anne.cosgrove@envato.com>
2022-01-04 17:29:45 +1100

    Extract universe initialisation and comment diagnostics

--- evolution.R
+++ evolution.R
@@ -1,3 +1,14 @@
+initialise <- function(width, height) {
+  hue <- raster(matrix(40, ncol = width, nrow = height)) 
+  sat <- raster(matrix((0:(width - 1)) * 100 / (width - 1), ncol = width, nrow = height, byrow=TRUE))
+  lum <- raster(matrix((0:(height - 1)) * 100 / (height - 1), ncol = width, nrow = height))
+  universe <- brick(hue, sat, lum)
+  names(universe) <- c("hue", "sat", "lum")
+  crs(universe) <- "+proj=longlat" # Remove warnings
+  extent(universe) <- c(xmin=0, xmax=1, ymin=0, ymax=height/width)
+  universe
+}
+
 evolve <- function(universe) {
   calc(universe, rotate_hue)
 }
--- src/automata.R
+++ src/automata.R
@@ -12,22 +12,16 @@ main <- function(width =10, height = 5) {
   key <- NA
   tkbind(tt,'<Key>', function(k) { key <<- k } )
   quit <- FALSE
-  hue <- raster(matrix(40, ncol = width, nrow = height)) 
-  sat <- raster(matrix((0:(width - 1)) * 100 / (width - 1), ncol = width, nrow = height, byrow=TRUE))
-  lum <- raster(matrix((0:(height - 1)) * 100 / (height - 1), ncol = width, nrow = height))
-  universe <- brick(hue, sat, lum)
-  names(universe) <- c("hue", "sat", "lum")
-  crs(universe) <- "+proj=longlat" # Remove warnings
-  extent(universe) <- c(xmin=0, xmax=1, ymin=0, ymax=height/width)
-  print(universe)
+  universe <- initialise(width, height)
+  # print(universe)
   
   while (!quit) {
     time <- system.time({
-      universe_rgb <<- calc(universe, hsl_stack_to_rgb)
+      universe_rgb <- calc(universe, hsl_stack_to_rgb)
       plotRGB(universe_rgb, r = 1, g = 2, b = 3)
       universe <- evolve(universe)
       names(universe) <- c("hue", "sat", "lum")
-      print(universe)
+      # print(universe)
       if(!is.na(key)) {
         print(paste("Key detected: ", key))
         if (key == 20) {
@@ -36,7 +30,7 @@ main <- function(width =10, height = 5) {
         key <- NA
       }
     })
-    print(mem_used())
+    # print(mem_used())
     print(time)
   }
   
