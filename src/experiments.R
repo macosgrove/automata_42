17fba5fa4e0d50be4715a7f6e9bc85e044aaa08a
macosgrove <mary-anne.cosgrove@envato.com>
2022-01-18 12:57:39 +1100

    Try 'adjacent'. Not helpful.

--- src/experiments.R
+++ src/experiments.R
@@ -10,20 +10,6 @@ data_from_stack <- stack(one, two, three)
 values(data_from_stack)
 extent(data_from_stack)
 
-data_from_brick <- brick(one, two, three)
-values(data_from_brick)
-extent(data_from_brick)
-
-# Region from stack works fine
-region_from_stack <- brick(data_from_stack, values=TRUE)
-values(region_from_stack)
-extent(region_from_stack)
-
-# Region from brick does not receive the data values
-region_from_brick <- brick(data_from_brick, values=TRUE)
-values(region_from_brick)
-extent(region_from_brick)
-
 # Use crop to extract a subregion. It always returns a brick
 # To crop by row and column numbers you can create an extent like this (for Raster x, row 5 to 10, column 7 to 12) crop(x, extent(x, 5, 10, 7, 12))
 ext <- extent(data_from_stack, 1, 3, 2, 3)
@@ -31,3 +17,8 @@ sub_region <- crop(data_from_stack, ext)
 values(sub_region)
 extent(sub_region)
 class(sub_region)
+
+# Try adjacent to extract a subregion
+adj <- adjacent(data_from_stack, cells=c(1, width*height), directions=8, pairs=FALSE) 
+as.matrix(adj)
+class(adj)
