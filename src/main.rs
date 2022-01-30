2fd5f26bd8bb5e5ed526e509cdc7173734dea6b8
macosgrove <mary-anne.cosgrove@envato.com>
2022-01-30 16:11:12 +1100

    Rust book chapter 2 - guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
