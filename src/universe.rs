f36a104bb43a892a755663a60f9b86e11db7059c
macosgrove <mary-anne.cosgrove@envato.com>
2022-07-07 16:57:09 +1000

    Add capacity to store generations to Universe

--- src/universe.rs
+++ src/universe.rs
@@ -2,23 +2,26 @@ use crate::image::Image;
 
 pub const UNIVERSE_WIDTH:usize = 300;
 pub const UNIVERSE_HEIGHT:usize = 200;
+const GENERATIONS:usize = 3;
 
 pub struct Universe {
-  cells: [[u32; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH],
+  cells: [[[u32; GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH],
+  generation: usize,
 }
 
 impl Universe {
   pub fn new(init: fn() -> u32) -> Self {
-    let cells: [[u32; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH] = [[init(); UNIVERSE_HEIGHT]; UNIVERSE_WIDTH];
+    let cells: [[[u32; GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH] = [[[init(); GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH];
     Universe {
       cells,
+      generation: 0
     }
   }
 
   pub fn evolve(&mut self, calc_next_gen: fn() -> u32) {
     for y in 0..UNIVERSE_HEIGHT-1 {
       for x in 0..UNIVERSE_WIDTH-1 {
-        self.cells[x][y] = calc_next_gen();
+        self.cells[x][y][self.generation] = calc_next_gen();
       }
     }
   }
@@ -27,7 +30,7 @@ impl Universe {
     let mut rendered: Image = Image::new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT);
     for y in 0..UNIVERSE_HEIGHT-1 {
       for x in 0..UNIVERSE_WIDTH-1 {
-          rendered.plot(x, y, self.cells[x][y]);
+          rendered.plot(x, y, self.cells[x][y][self.generation]);
       }
     }
     rendered
