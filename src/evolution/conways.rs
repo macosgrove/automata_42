c94828e601f235b343dbfe37af60f8e283bc7b75
macosgrove <mary-anne.cosgrove@envato.com>
2022-07-12 11:18:56 +1000

    Progress commit

--- src/evolution/conways.rs
+++ src/evolution/conways.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
+
+use crate::universe::Generation;
 pub fn init() -> u32 {
   return 0
 }
 
-pub fn evolve() -> u32 {
+pub fn evolve(last_generation: Box<Generation>, x:usize, y:usize) -> u32 {
   let colour:u32;
   if rand::random() {
     colour = 0xffffff;
--- src/universe.rs
+++ src/universe.rs
@@ -4,33 +4,47 @@ pub const UNIVERSE_WIDTH:usize = 300;
 pub const UNIVERSE_HEIGHT:usize = 200;
 const GENERATIONS:usize = 3;
 
+pub type Generation = [[u32; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH];
+
+#[derive(Debug)]
 pub struct Universe {
-  cells: [[[u32; GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH],
+  generations: [Box<Generation>; GENERATIONS],
   generation: usize,
 }
 
 impl Universe {
   pub fn new(init: fn() -> u32) -> Self {
-    let cells: [[[u32; GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH] = [[[init(); GENERATIONS]; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH];
+    let mut generations: [Box<Generation>; GENERATIONS] = [allocate_generation(); GENERATIONS];
+
+    fn allocate_generation() -> Box<Generation> {
+      Box::new([[0; UNIVERSE_HEIGHT]; UNIVERSE_WIDTH])
+    }
+
     Universe {
-      cells,
+      generations,
       generation: 0
     }
   }
 
-  pub fn evolve(&mut self, calc_next_gen: fn() -> u32) {
+
+
+  pub fn evolve(&mut self, calc_next_gen: fn(&Box<Generation>, usize, usize) -> u32) {
+    let last_generation = &self.generations[self.generation];
+    self.generation = (self.generation + 1) % GENERATIONS;
+    let mut current_generation = *self.generations[self.generation];
     for y in 0..UNIVERSE_HEIGHT-1 {
       for x in 0..UNIVERSE_WIDTH-1 {
-        self.cells[x][y][self.generation] = calc_next_gen();
+        current_generation[x][y] = calc_next_gen(last_generation, x, y);
       }
     }
   }
 
   pub fn render(&mut self) -> Image {
     let mut rendered: Image = Image::new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT);
+    let current_generation = *self.generations[self.generation];
     for y in 0..UNIVERSE_HEIGHT-1 {
       for x in 0..UNIVERSE_WIDTH-1 {
-          rendered.plot(x, y, self.cells[x][y][self.generation]);
+          rendered.plot(x, y, current_generation[x][y]);
       }
     }
     rendered
